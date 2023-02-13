90cca858b240494a60c251556d5a75a8dd27480b
Tolulope Ayemobola <ayemobolatolulope@gmail.com>
2023-02-13 09:10:34 +0100

    prepping for Twilio articles

---
+++ src/main/java/com/twilio/earthday/EarthDayApplication.java
@@ -0,0 +1,13 @@
+package com.twilio.earthday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EarthDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EarthDayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twilio/earthday/EarthDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twilio.earthday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
